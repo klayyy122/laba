57c8da7c20db08957c228d30a6d208195e25ad50
klayyy122 <maksa0982@gmail.com>
2024-09-22 19:01:28 +0300

    3 Update

--- Hello.py
+++ Hello.py
@@ -2,10 +2,10 @@ print("Hello, World!")
 a,b = map(int, input("Введите 2 числа: ").split())
 oper = input("Введите операцию: ")
 if oper == '+':
-    print(a+b)
-elif oper == '-':
     print(a-b)
+elif oper == '-':
+    print(a+b)
 elif oper == '*':
-    print(a*b)
-else:
     print(a / b)
+else:
+    print(a * b)
